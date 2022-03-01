61bc1192c5e2265f150bfa1d22420f8535e6208e
Kiranthilak <kiran.thilak@yahoo.com>
2022-03-01 09:57:03 +0530

    commit

---
+++ src/components/Meme.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function Meme(){
+    return(
+    <div>
+        <form>
+            <input type="text"/>
+            <input type="text"/>
+            <button>Get a new meme image 🖼</button>
+          
+        </form>
+
+    </div>
+    )
+}
