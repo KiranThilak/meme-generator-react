ad05c62342b797e25ec0e26ff8b8eb9e82a506ad
Kiranthilak <kiran.thilak@yahoo.com>
2022-03-03 05:10:18 +0530

    get random image from array

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,40 @@
 import React from "react";
+import memesData from "./memesData";
 
 
 export default function Meme(){
-    return(
-    <div>
-        <form>
-            <input type="text"/>
-            <input type="text"/>
-            <button>Get a new meme image 🖼</button>
-          
-        </form>
 
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random () * memesArray.length )
+        const randomImage = memesArray[randomNumber].url
+
+        console.log(randomImage)
+
+
+
+
+    }
+
+
+    return(
+        <div className="form">
+        <input 
+            type="text"
+            placeholder="Top text"
+            className="form--input"
+        />
+        <input 
+            type="text"
+            placeholder="Bottom text"
+            className="form--input"
+        />
+        <button 
+            className="form--button"
+            onClick={getMemeImage}
+        >
+            Get a new meme image 🖼
+        </button>
     </div>
     )
 }
